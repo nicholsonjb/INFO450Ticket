4672118a2dc157ef61723176faca14f1926b0941
James Nicholson <nicholsonjb@vcu.edu>
2017-03-03 20:32:27 -0500

    Do while loop works

--- INFO450Ticket/INFO450Ticket.cpp
+++ INFO450Ticket/INFO450Ticket.cpp
@@ -26,7 +26,7 @@ public:
 	void CaptureTicket();
 	void ShowTicket();
 	void CloseTicket();
-	bool continueTicket();
+	/*bool continueTicket();*/
 
 
 };
@@ -48,15 +48,21 @@ int Ticket::getID()
 //Metod to CaptureTicket Info
 void Ticket::CaptureTicket()
 {
+	char ans;
 	
-	while (continueTicket()) 
-	{
-		cin.clear();
+
+	do 
+	{	
+		cout << "Open new ticket?  enter Y to continue Press. " << endl;
+		cin.get(ans);
 		cin.ignore();
-		cout << "Enter Customer Name: " << endl;
+		
+		cout << "Enter Customer Name:" << endl;
 		getline(cin, name);
-		cout << "Issue type: Issue Type? S=Server, A=Application, C=aCcess " << endl;
+
+		cout << "Issue type: Issue Type? S=Server, A=Application, C=aCcess" << endl;
 		getline(cin, ticketIssueType);
+
 		if (ticketIssueType == "S" || ticketIssueType == "s")
 		{
 			issueType = "Server";
@@ -70,12 +76,13 @@ void Ticket::CaptureTicket()
 			issueType = "Access";
 		}
 
-		cout << "Description of Issue? " << endl;
+		cout << "Description of Issue?" << endl;
 		cout << issueType << endl;
 		getline(cin, ticketDescription);
 
-		cout << "How many users impacted? " << endl;
+		cout << "How many users impacted?" << endl;
 		cin >> usersImpacted;
+
 		if (usersImpacted < 10)
 		{
 			ticketPriority = "Low";
@@ -88,11 +95,12 @@ void Ticket::CaptureTicket()
 		{
 			ticketPriority = "HIGH";
 		}
-		cout << "Your issue ID is : " << ID << endl;
-		cout << "--------------------------\n\n";
-		cin.clear();
+
 		cin.ignore();
-	}
+		cout << "Your issue ID is: " << ID << endl;
+		cout << "--------------------------" << endl;
+		
+	} while (ans !='n' && ans !='n');
 	
 }
 
@@ -123,14 +131,15 @@ void Ticket::CloseTicket()
 }
 
 //Method  to continue
-bool Ticket::continueTicket()
-{
-
-	cout << "Open new ticket?  enter Y to continue Press. \n"
-		<< "Or press 0 to stop: \n";
-	return (cin.get() != '0');
-
-}
+//bool Ticket::continueTicket()
+//{
+//
+//	cout << "Open new ticket?  enter Y to continue Press. \n"
+//		<< "Or press 0 to stop: \n";
+//	
+//	return (cin.get() != '0');
+//	
+//}
 
 
 int main()
@@ -144,10 +153,12 @@ int main()
 	for (int i = 0; i < MAXTICKETS; i++)
 	{
 		myTickets[i].CaptureTicket();
-
-			myTickets[i].ShowTicket();
-			myTickets[0].CloseTicket();
-			}
+	}
+	for (int i = 0; i < MAXTICKETS; i++)
+	{
+		myTickets[i].ShowTicket();
+		myTickets[0].CloseTicket();
+	}
 	
 
 	return 0;
